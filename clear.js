5e02079dd4d1ee30b7820505edeb5ae89299d9e1
PREMKUMARPDP <premkumar47227@gmail.com>
2024-05-01 15:10:15 +0530

    Login

---
+++ clear.js
@@ -0,0 +1,11 @@
+function sub(){
+    var name = document.getElementById('name').value;
+    var Password = document.getElementById('Password').value;
+//two way to store dta in localstorage
+//1
+var getname = localStorage.getItem('name');
+var getPassword = localStorage.getItem('Password');
+if(entername == getname){
+    if(enterPassword == getPassword);
+}
+}
